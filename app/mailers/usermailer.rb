f16180904116153247785e8a155e57cd1b3b6a70
Tom <dongtangjin@gmail.com>
2014-12-04 16:54:50 +0800

    设置gmail smpt服务,邮件发送测试通过

--- app/mailers/usermailer.rb
+++ app/mailers/usermailer.rb
@@ -1,3 +1,10 @@
 class Usermailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "coul201306@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    email_with_name = "#{@user.name} <#{@user.email}>"
+    @url = "127.0.0.1/login"
+    mail(to:@user.email,subject: "Welcome to my awesome site")
+  end
 end
